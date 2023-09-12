3b64f8b0c01943aa2225eae886fbd0f2455436be
Alserial <95892981+Alserial@users.noreply.github.com>
2023-09-12 23:58:27 +1000

    Music API

--- back-end/MusicPlayerApp/urls.py
+++ back-end/MusicPlayerApp/urls.py
@@ -6,5 +6,8 @@ from django.conf import settings
 urlpatterns = [
     path('musiclist/', views.musiclistApi),
     re_path(r'^musiclist/(?P<id>[0-9]*)$', views.musiclistApi),
-    path('upload/', views.SaveFile)
+    path('upload/', views.SaveFile),
+    # music 
+    path('music/', views.musicApi),
+    re_path(r'^music/(?P<id>[0-9]*)$', views.musicApi),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- back-end/MusicPlayerApp/views.py
+++ back-end/MusicPlayerApp/views.py
@@ -6,11 +6,41 @@ from rest_framework.parsers import JSONParser
 
 from django.http.response import JsonResponse
 
-from MusicPlayerApp.models import MusicList
+from MusicPlayerApp.models import MusicList,Musics
 
-from MusicPlayerApp.serializers import MusicListSerializer
+from MusicPlayerApp.serializers import MusicListSerializer,MusicSerializer
 
 from django.core.files.storage import default_storage
+
+@csrf_exempt
+def musicApi(request,id=0):
+    if request.method=='GET':
+        musics = Musics.objects.all()
+        music_serializer = MusicSerializer(musics, many=True) 
+        return JsonResponse (music_serializer. data, safe=False)
+    
+    elif request.method=='POST':
+        musics_data=JSONParser().parse(request)
+        musics_serializer = MusicSerializer(data=musics_data) 
+        if musics_serializer.is_valid():
+            musics_serializer.save()
+            return JsonResponse("Added Successfully!!" , safe=False)
+        return JsonResponse("Failed to Add.", safe=False)
+
+    elif request.method=='PUT':
+        musics_data = JSONParser().parse(request)
+        music = Musics.objects.get(MusicID=musics_data['MusicID']) 
+        musics_serializer = MusicSerializer(music, data=musics_data)
+        if musics_serializer.is_valid():
+            musics_serializer.save()
+            return JsonResponse("Updated Successfully!!", safe=False) 
+        return JsonResponse("Failed to Update.", safe=False) 
+
+    elif request.method=='DELETE':
+        music = Musics.objects.get(MusicID=id)
+        music.delete()
+        return JsonResponse ("Deleted Successfully!!", safe=False)
+    
 
 @csrf_exempt
 def musiclistApi(request,id=0):
