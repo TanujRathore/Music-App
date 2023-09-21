a52a467d81caae3841bc93e1288c8fa4ee319285
Olivia Fan <oliviafan0127@gmail.com>
2023-09-21 14:01:39 +1000

    Update database with connection

--- back-end/MusicPlayerApp/models.py
+++ back-end/MusicPlayerApp/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 
 # Create your models here.
-class MusicList(models.Model):
-    MusicListId = models.AutoField(primary_key=True)
-    MusicListName = models.CharField(max_length=100)
-    MusicListProfilePic = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    
 class Musics(models.Model):
-    MusicID = models.AutoField(primary_key=True)
-    MusicName = models.CharField(max_length=100)
+    musicID = models.AutoField(primary_key=True)
+    musicName = models.CharField(max_length=100)
+    musicUrl = models.CharField(max_length=100)
+    musicType = models.CharField(max_length=100, null=True, blank=True)
+    
 class UserRole(models.Model):
     username = models.CharField(max_length=100,primary_key=True)
     role = models.CharField(max_length=100, default="user")
     firstname = models.CharField(max_length=100, default="user")
     lastname = models.CharField(max_length=100, default="user")
+    
+class MusicList(models.Model):
+    musicListId = models.AutoField(primary_key=True)
+    musicListName = models.CharField(max_length=100, default='favourite')
+    musicListProfilePic = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    userBelongTo = models.IntegerField()
+    musicIn = models.ManyToManyField(Musics)
+    
