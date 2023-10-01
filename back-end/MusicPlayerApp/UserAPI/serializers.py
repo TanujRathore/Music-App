395d6d84308bf49058eb27b5de75821ea36ba383
Alserial <95892981+Alserial@users.noreply.github.com>
2023-10-01 22:24:03 +0800

    add MusIc List field in User

--- back-end/MusicPlayerApp/UserAPI/serializers.py
+++ back-end/MusicPlayerApp/UserAPI/serializers.py
@@ -1,7 +1,10 @@
-# serializers.py
-
 from rest_framework import serializers
-from MusicPlayerApp.models import UserRole
+from MusicPlayerApp.models import UserRole, MusicList, Musics
+
+class MusicListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MusicList
+        fields = ('musicListId', 'musicListName', 'userBelongTo', 'musicIn')
 
 class UserRoleSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,6 +13,7 @@ class UserRoleSerializer(serializers.ModelSerializer):
                   'role',
                   'firstname',
                   'lastname')
+
     def validate_username(self, value):
         return value.lower()
     
@@ -18,3 +22,15 @@ class UserRoleSerializer(serializers.ModelSerializer):
     
     def validate_lastname(self, value):
         return value.lower()
+
+    def create(self, validated_data):
+        # create the user role instance
+        user_instance = UserRole.objects.create(**validated_data)
+        
+        # create the five default music lists for this user
+        MusicList.objects.create(musicListName="Favourite", userBelongTo=user_instance.username)
+        MusicList.objects.create(musicListName="Favourite1", userBelongTo=user_instance.username)
+        MusicList.objects.create(musicListName="Favourite2", userBelongTo=user_instance.username)
+        MusicList.objects.create(musicListName="Favourite3", userBelongTo=user_instance.username)
+        MusicList.objects.create(musicListName="Favourite4", userBelongTo=user_instance.username)
+        return user_instance
--- back-end/MusicPlayerApp/models.py
+++ back-end/MusicPlayerApp/models.py
@@ -7,17 +7,19 @@ class Musics(models.Model):
     musicName = models.CharField(max_length=100)
     musicUrl = models.CharField(max_length=100, null=True, blank=True)
     musicType = models.CharField(max_length=100, null=True, blank=True)
-    
-class UserRole(models.Model):
-    username = models.CharField(max_length=100,primary_key=True)
-    role = models.CharField(max_length=100, default="user")
-    firstname = models.CharField(max_length=100, default="user")
-    lastname = models.CharField(max_length=100, default="user")
-    
+
 class MusicList(models.Model):
     musicListId = models.AutoField(primary_key=True)
     musicListName = models.CharField(max_length=100, default='favourite')
     musicListProfilePic = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
     userBelongTo = models.CharField(max_length=100)
     musicIn = models.ManyToManyField(Musics)
+    
+class UserRole(models.Model):
+    username = models.CharField(max_length=100,primary_key=True)
+    role = models.CharField(max_length=100, default="user")
+    firstname = models.CharField(max_length=100, default="user")
+    lastname = models.CharField(max_length=100, default="user")
+    musicListIn = models.ManyToManyField(MusicList)
+    
     
