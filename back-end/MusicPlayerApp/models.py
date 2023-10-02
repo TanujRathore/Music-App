f2290264d396290a8a5c2d05c7fbe413eb3e53e8
Olivia Fan <oliviafan0127@gmail.com>
2023-10-02 21:02:38 +1100

    update key name in models

--- back-end/MusicPlayerApp/models.py
+++ back-end/MusicPlayerApp/models.py
@@ -6,7 +6,7 @@ class Music(models.Model):
     musicID = models.AutoField(primary_key=True)
     musicName = models.CharField(max_length=100)
     musicUrl = models.CharField(max_length=100, null=True, blank=True)
-    musicType = models.CharField(max_length=100, null=True, blank=True)
+    musicAuthor = models.CharField(max_length=100, null=True, blank=True)
 
 class UserRole(models.Model):
     username = models.CharField(max_length=100,primary_key=True)
@@ -20,7 +20,4 @@ class MusicList(models.Model):
     musicListProfilePic = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
     userBelongTo = models.ForeignKey(UserRole, on_delete=models.CASCADE, related_name="music_lists")
     musicIn = models.ManyToManyField(Music)
-    
-
-    
     
