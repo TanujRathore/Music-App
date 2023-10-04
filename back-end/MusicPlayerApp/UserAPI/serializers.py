8ca7b3932f047fe29bcc758509340ce08c81bb82
Alserial <95892981+Alserial@users.noreply.github.com>
2023-10-05 00:34:43 +1100

    Added error rollback on user creation

--- back-end/MusicPlayerApp/UserAPI/serializers.py
+++ back-end/MusicPlayerApp/UserAPI/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from MusicPlayerApp.models import UserRole, MusicList
+from django.db import transaction
+
 
 class MusicListSerializer(serializers.ModelSerializer):
     class Meta:
@@ -24,13 +26,19 @@ class UserRoleSerializer(serializers.ModelSerializer):
         return value.lower()
 
     def create(self, validated_data):
-        # create the user role instance
-        user_instance = UserRole.objects.create(**validated_data)
-        
-        # create the five default music lists for this user
-        MusicList.objects.create(musicListName="Favourite", userBelongTo=user_instance)
-        MusicList.objects.create(musicListName="Morning Motivation", userBelongTo=user_instance, musicListProfilePic="MorningMotivation")
-        MusicList.objects.create(musicListName="Daily Activity", userBelongTo=user_instance, musicListProfilePic="DailyActivity")
-        MusicList.objects.create(musicListName="Afternoon Relaxation", userBelongTo=user_instance, musicListProfilePic="AfternoonRelaxation")
-        MusicList.objects.create(musicListName="Sleep Preparation", userBelongTo=user_instance, musicListProfilePic="SleepPreparation")
+        try:
+            with transaction.atomic():
+                # create the user role instance
+                user_instance = UserRole.objects.create(**validated_data)
+                
+                # create the five default music lists for this user
+                if user_instance.role == "staff":
+                    MusicList.objects.create(musicListName="Favourite", userBelongTo=user_instance)
+                    MusicList.objects.create(musicListName="Morning Motivation", userBelongTo=user_instance, musicListProfilePic="MorningMotivation")
+                    MusicList.objects.create(musicListName="Daily Activity", userBelongTo=user_instance, musicListProfilePic="DailyActivity")
+                    MusicList.objects.create(musicListName="Afternoon Relaxation", userBelongTo=user_instance, musicListProfilePic="AfternoonRelaxation")
+                    MusicList.objects.create(musicListName="Sleep Preparation", userBelongTo=user_instance, musicListProfilePic="SleepPreparation")
+        except Exception as e:
+            print(f"Error creating user and music lists: {str(e)}")
+
         return user_instance
