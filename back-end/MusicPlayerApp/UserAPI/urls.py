22413429e81e096fcd496841e745ddf05d8c8851
Alserial <95892981+Alserial@users.noreply.github.com>
2023-10-19 20:42:19 +1100

    add userRemove function

--- back-end/MusicPlayerApp/UserAPI/urls.py
+++ back-end/MusicPlayerApp/UserAPI/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('manage/', views.manageAPI, name='manage-api'),
-    re_path(r'manage/(?P<name>[a-zA-Z]+)$', views.manageAPI, name='manage-detail-api'),
+    re_path(r'manage/(?P<name>\w+)$', views.manageAPI, name='manage-detail-api'),
     path('login/', views.loginAPI, name='login-api'),
 ]
